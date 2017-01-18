4bf69a8378c01d59969df2e3f12a0a2423c5730b
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-01-18 11:35:08 +0100

    improve standalone usability

---
+++ set_environment.csh
@@ -0,0 +1,7 @@
+#!/bin/tcsh
+
+# set path to look python bits for
+echo "adding base Beamspot directory to PATH"
+echo $PWD
+setenv PATH $PWD:$PATH
+
---
+++ set_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# set path to look python bits for
+echo "adding base Beamspot directory to PATH"
+echo $PWD
+export PATH=$PWD:$PATH
+
