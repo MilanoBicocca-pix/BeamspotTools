f0dda6ad29652a72ff569df60cc00f0884d198b8
Riccardo Manzoni <riccardo.manzoni@cern.ch>
2017-04-28 12:20:34 +0200

    make sure that the __init__.py files sit were they should

--- set_environment.csh
+++ set_environment.csh
@@ -6,3 +6,10 @@ echo $PWD
 setenv PYTHONPATH $PWD:$PYTHONPATH
 setenv BSBASE $PWD
 
+# also touch __init__.py files, just in case
+touch data/__init__.py
+touch objects/__init__.py
+touch plot/__init__.py
+touch utils/__init__.py
+touch test/__init__.py
+
--- set_environment.sh
+++ set_environment.sh
@@ -5,3 +5,11 @@ echo "adding base Beamspot directory to PYTHONPATH"
 echo $PWD
 export PYTHONPATH=$PWD:$PYTHONPATH
 export BSBASE=$PWD
+
+# also touch __init__.py files, just in case
+touch data/__init__.py
+touch objects/__init__.py
+touch plot/__init__.py
+touch utils/__init__.py
+touch test/__init__.py
+
