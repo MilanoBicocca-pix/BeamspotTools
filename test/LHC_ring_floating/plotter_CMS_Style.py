731d75c5b6edd203bd3543808cb236642de93022
francescobrivio <francesco.brivio@cern.ch>
2024-03-14 13:56:56 +0100

    update historical plot up to September 2023

--- test/LHC_ring_floating/plotter_CMS_Style.py
+++ test/LHC_ring_floating/plotter_CMS_Style.py
@@ -68,6 +68,7 @@ times = [
     'year 2023, month 5',
     'year 2023, month 6',
     'year 2023, month 7',
+    'year 2023, month 9'
 ]
 
 Time = [datetime.strptime(time,"year 20%y, month %m") for time in times ]
@@ -135,7 +136,8 @@ X = np.array([
     0.115344,#2023
     0.116808,
     0.117166,
-    0.116243
+    0.116243,
+    0.112538
 ])
 
 Y = np.array([
@@ -201,7 +203,8 @@ Y = np.array([
     -0.188842,#2023
     -0.186396,
     -0.183765,
-    -0.181976
+    -0.181976,
+    -0.186970
 ])
 
 # convert from cm to micron
@@ -232,7 +235,8 @@ plt.xticks(rotation = 0, ha='center')
 # m, b = np.polyfit(Time, Y, 1)
 # plt.plot(x, m*x + b, '-', c='g')
 # plt.show()
-plt.savefig('cms_beamspotXY_vs_time_2023_July.pdf')
+plt.savefig('cms_beamspotXY_vs_time_2023_September.pdf')
+plt.savefig('cms_beamspotXY_vs_time_2023_September.png')
 plt.close()
 
 
@@ -286,3 +290,4 @@ plt.close()
 # year 2023, month 5    X Pos = 0.116808 - Width = 7.8006E-04 [cm]   Y Pos = -0.186396 - Width = 8.5948E-04 [cm]   Z Pos = -0.600535 - Width = 7.8904E-01 [cm]
 # year 2023, month 6    X Pos = 0.117145 - Width = 8.4810E-04 [cm]   Y Pos = -0.183745 - Width = 9.4802E-04 [cm]   Z Pos = -0.430677 - Width = 3.7080E+00 [cm]
 # year 2023, month 7    X Pos = 0.116243 - Width = 7.7234E-04 [cm]   Y Pos = -0.181976 - Width = 8.3111E-04 [cm]   Z Pos = -0.519375 - Width = 3.6933E+00 [cm]
+# year 2023, month 9    X Pos = 0.112538 - Width = 6.5062E-03 [cm]   Y Pos = -0.186970 - Width = 6.5946E-03 [cm]   Z Pos = -0.278197 - Width = 4.5345E+00 [cm]
